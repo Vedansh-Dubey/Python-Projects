307abd8ba655cb2fc6838f70f38ff8d577ecdc39
Vedansh Dubey <vedanshstudy@gmail.com>
2021-12-29 22:28:36 +0530

    basic program 1

---
+++ Areaofcircle.py
@@ -0,0 +1,8 @@
+import  math
+r=float(input("Enter the radius of circle: "))
+Area= math.pi*r*r
+print("Area of circle is: ",Area)
+Circum= math.pi*2*r
+print("Circumference of circle is: ",Circum)
+arc=math.pi*r
+print("Arc length of circle is: ",arc)
